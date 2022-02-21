408dff86b45980ac6ab69bfc23298ef93764dd0a
Jerard10 <100113359+Jerard10@users.noreply.github.com>
2022-02-21 04:54:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,13 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
-    <head>
-          <title>My Home Page</title>
+  <head>
+    <title>My Home Page</title>
     </head>
-    <body>
-    <h1>Welcome to my Website! I am Jerard Cordero! </h1>
+  <body>
+    <h1>Welcome to My Website! Jerard Cordero!</h1>
     <ul>
-    <li><a href="index.php">HOME</a></li>
-    <li><a href="home1.html" target="_blank" ><li>home</li></a>
-    <li><a href="Chili%20con%20Carne.html" target="_blank" ><li>Chili con Carne</li></a>
-<li><a href="Beef%20on%20Beers.html"target="_blank" ><li>Beef in Beer</li></a>
-<li><a href="Pineapple%20Chicken.html" target="_blank" ><li>Pineapple Chicken</li></a>
-<li><a href="Carbonara%20al%20funghi.html" target="_blank" ><li>Carbonara al funghi </li></a> 
-<li><a href="Goat%20cheese%20and%20Beetroot%20salad.html" target="_blank" ><li>Goat cheese and Beetroot salad </li></a> 
-<li><a href="Cucumber%20salad.html" target="_blank" ><li>Cucumber salad </li></a>
-<li><a href="Salmon%20in%20the%20oven.html"target="_blank" ><li>Salmon in the oven </li></a>    
+      <li><a href="index.php">HOME</a></li>
+      <li><a href="LT2/Index.html">Learning Task 2</a></li>
     </ul>
-    </body>
+  </body>
 </html>
