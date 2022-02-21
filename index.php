afbe3a7b11a3962dbb452d9ec22984772d78b7fe
Jerard10 <100113359+Jerard10@users.noreply.github.com>
2022-02-21 03:13:35 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
     <ul>
     <li><a href="index.php">HOME</a></li>
     <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
-    <a href="file:///C:/Users/HP/Documents/Jerard/Chili%20con%20Carne.html" target="_blank" ><li>Chili con Carne</li></a>
     </ul>
     </body>
 </html>
