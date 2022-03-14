02fa79b94daf66cd45045979cc136657b365efe0
Jerard10 <100113359+Jerard10@users.noreply.github.com>
2022-03-14 05:30:07 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <!DOCTYPE HTML>
 <html>
   <head>
-    <title>My Home Page</title>
     </head>
   <body>
-    <h1>Welcome to My Website! Jerard Cordero!</h1>
     <ul>
       <li><a href="index.html">Learning Task 3</a></li>
     </ul>
