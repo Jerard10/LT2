98e8229965d6efa64bc010fc55f263eb85fab49e
Jerard10 <100113359+Jerard10@users.noreply.github.com>
2022-03-14 06:08:41 -0400

    Update index.php

--- index.php
+++ index.php
@@ -11,38 +11,38 @@
     <h1>Francine's Top 10 songs</h1>
 <div id="wrapper">
         <div id="box-one">
-             <h2>Top 1</h2><img src="16b.jpg" alt="16b by James reid" width="200" height="200"><h3>16b</h3><p class="text-content-one">16b is all about the guys that falling in love with girl but is too much dangerous and toxic and 16b is the place where all that happens. the song is based on a reality. </p>
+             <h2>Top 1</h2><img src="16b.jpg" alt="16b by James reid" width="200" height="200"><h3>16b by James Reid</h3><p class="text-content-one">16b is all about the guys that falling in love with girl but is too much dangerous and toxic and 16b is the place where all that happens. the song is based on a reality. </p>
         </div>
         
          <div id="box-two">
-             <h2>Top 2</h2><img src="robbers.jpg" alt="Robber by 1975" width="200" height="200"><h3>Robbers</h3><p class="text-content-one">This song is about two lovers who aren't good for each other. She's having second thoughts but they keep putting off the inevitable end of the relationship. In doing so they are robbing each other of happiness.</p>
+             <h2>Top 2</h2><img src="robbers.jpg" alt="Robber by 1975" width="200" height="200"><h3>Robbers by 1975</h3><p class="text-content-one">This song is about two lovers who aren't good for each other. She's having second thoughts but they keep putting off the inevitable end of the relationship. In doing so they are robbing each other of happiness.</p>
         </div>
     <div id="box-three">
-             <h2>Top 3</h2><img src="all%20too%20well.jpg" alt="All too well by Taylor Swift" width="200" height="200"><h3>All Too Well</h3><p class="text-content-one">the 10-minute version of "All Too Well" has been a fandom in joke/meme we're actually kinda serious about wanting to hear it tho ever since.</p>
+             <h2>Top 3</h2><img src="all%20too%20well.jpg" alt="All too well by Taylor Swift" width="200" height="200"><h3>All Too Well by Taylor Swift</h3><p class="text-content-one">the 10-minute version of "All Too Well" has been a fandom in joke/meme we're actually kinda serious about wanting to hear it tho ever since.</p>
         </div>
     <div id="box-four">
-             <h2>Top 4</h2><img src="praise%20the%20lord.jpg" alt="Praise The Lord by Asap Rocky" width="200" height="200"><h3>Praise The Lord</h3><p class="text-content-one">A$AP Rocky has been a avid drug user since his teenage years when he sold weed and crack in the Bronx. The rapper's open attitude towards his usage of pharmaceuticals feeds into his music as well, once even recording a track titled "LSD." Rocky laid down this cut with UK grime artist Skepta in London while the pair were high on mushrooms and LSD.</p>
+             <h2>Top 4</h2><img src="praise%20the%20lord.jpg" alt="Praise The Lord by Asap Rocky" width="200" height="200"><h3>Praise The Lord by Asap Rocky</h3><p class="text-content-one">A$AP Rocky has been a avid drug user since his teenage years when he sold weed and crack in the Bronx. The rapper's open attitude towards his usage of pharmaceuticals feeds into his music as well, once even recording a track titled "LSD." Rocky laid down this cut with UK grime artist Skepta in London while the pair were high on mushrooms and LSD.</p>
         </div>
     <div id="box-five">
-             <h2>Top 5</h2><img src="love.jpg" alt="Love. by Kendrick Lamar" width="200" height="200"><h3>All Of Us Are Dead</h3><p class="text-content-one">“LOVE.” finds Kendrick Lamar musing on the woman in his life; presumably his fiancé Whitney Alford, whom he has been in a relationship with since his high school days. Kendrick uses this ballad to ask the essential questions to his partner to solidify the most important aspect of their relationship: love.
+             <h2>Top 5</h2><img src="love.jpg" alt="Love. by Kendrick Lamar" width="200" height="200"><h3>Love by Kendrick Lamar</h3><p class="text-content-one">“LOVE.” finds Kendrick Lamar musing on the woman in his life; presumably his fiancé Whitney Alford, whom he has been in a relationship with since his high school days. Kendrick uses this ballad to ask the essential questions to his partner to solidify the most important aspect of their relationship: love.
 
              Kendrick has kept his relationship with Alford under wraps for a majority of his career. It wasn’t until 2016 that word got out that he was engaged.</p>
         </div>
     <div id="box-six">
-             <h2>Top 6</h2><img src="summer.jpg" alt="Summer by James and Nadine" width="200" height="200"><h3>Summer</h3><p class="text-content-one">the song is all about "LIFE".</p>
+             <h2>Top 6</h2><img src="summer.jpg" alt="Summer by James and Nadine" width="200" height="200"><h3>Summer by James and Nadine</h3><p class="text-content-one">the song is all about "LIFE".</p>
         </div>
     <div id="box-seven">
-             <h2>Top 7</h2><img src="paris.jpg" alt="Paris by Chainsmoker" width="200" height="200"><h3>Paris</h3><p class="text-content-one">The Chainsmokers expanded on the background to the song during a Facebook post:
+             <h2>Top 7</h2><img src="paris.jpg" alt="Paris by Chainsmoker" width="200" height="200"><h3>Paris by Chainsmoker</h3><p class="text-content-one">The Chainsmokers expanded on the background to the song during a Facebook post:
              "Drew had a friend from the East coast who had been struggling with a drug addiction for the past few years. He was a childhood friend and their families were close, so his parents kept him informed on how he was doing. He and Drew would keep in touch on Facebook chat but would never speak about what he was going through, but meanwhile Drew knew of all the craziness that surrounded his friend's life.</p>
         </div>
     <div id="box-eight">
-             <h2>Top 8</h2><img src="this%20feeling.jpg" alt="This Feeling by Chainsmoker" width="200" height="200"><h3>This Feeling</h3><p class="text-content-one">This is a story of how two people, who are wrong for each other, force themselves into a relationship. Their friends can see that these two are not meant for each other, but the couple fails to fight their instant gut feelings.
+             <h2>Top 8</h2><img src="this%20feeling.jpg" alt="This Feeling by Chainsmoker" width="200" height="200"><h3>This Feeling by Chainsmoker</h3><p class="text-content-one">This is a story of how two people, who are wrong for each other, force themselves into a relationship. Their friends can see that these two are not meant for each other, but the couple fails to fight their instant gut feelings.
              The girl says they have certain expectations of their relationship. At a glance, it seems as if the partner is able to cater to all these requirements. But that’s what ‘the thing in their chest’ says. Not that ‘the thing in your chest’ is always wrong. But you have to give reigns to the head when jumping into a relationship.</p>
         </div>
     <div id="box-nine">
-             <h2>Top 9</h2><img src="zombie.jpg" alt="Zombie by Cranberries" width="200" height="200"><h3>Zombie</h3><p class="text-content-one"> The song was written in response to the death of Johnathan Ball, 3, and Tim Parry, 12, who had been killed in the IRA bombing in Warrington, northwest England, when two devices hidden in litter bins were detonated.</p>
+             <h2>Top 9</h2><img src="zombie.jpg" alt="Zombie by Cranberries" width="200" height="200"><h3>Zombie by Cranberries</h3><p class="text-content-one"> The song was written in response to the death of Johnathan Ball, 3, and Tim Parry, 12, who had been killed in the IRA bombing in Warrington, northwest England, when two devices hidden in litter bins were detonated.</p>
         </div>
     <div id="box-ten">
-             <h2>Top 10</h2><img src="something%20new.jpg" alt="Something New by Zendaya" width="200" height="200"><h3>Something New</h3><p class="text-content-one">the is all about she want to try something new and no one can stop her. </p>
+             <h2>Top 10</h2><img src="something%20new.jpg" alt="Something New by Zendaya" width="200" height="200"><h3>Something New by Zendaya</h3><p class="text-content-one">the is all about she want to try something new and no one can stop her. </p>
         </div>
     </div>
